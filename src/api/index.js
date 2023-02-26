87d6f8394488d99c181a6f856a2e5f8b4cbcd974
unknown <you@example.com>
2023-02-26 21:35:06 +0800

    search 页面的接口请求渲染

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,12 @@
+/*
+ * @Copyright: Copyright© 2022 AOMEI
+ * @Abstract: 
+ * @Date: 2023-02-26 08:54:51
+ * @Author: 
+ * @LastEditors: houliucun
+ * @LastEditTime: 2023-02-26 16:36:29
+ * @RevisionHistory: 
+ */
 // 当前的接口是对api进行统一的管理
 import requests from "./requests"
 import mockRequests from "./mockAjax"
@@ -16,3 +25,10 @@ export const reqGetBannerList = () => mockRequests.get("/banner")
 // 获取floor组件的数据
 
 export const reqGetFloorList = () => mockRequests.get("/floor")
+
+// 获取搜索模块
+export const reqGetSearchInfo = (params) => requests({
+    url: "/list",
+    method: 'post',
+    data: params
+})
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -4,20 +4,38 @@
  * @Date: 2023-02-21 23:05:19
  * @Author: 
  * @LastEditors: houliucun
- * @LastEditTime: 2023-02-23 21:34:11
+ * @LastEditTime: 2023-02-26 21:03:15
  * @RevisionHistory: 
  */
+import { reqGetSearchInfo } from "@/api/index"
 const state = {
-
+    searchList: {}
 }
 const mutations = {
-
+    REQGETSEARCHLIST(state, searchList) {
+        state.searchList = searchList
+    }
 }
 const actions = {
-
+    async reqGetSearchList({ commit }, params = {}) {
+        let result = await reqGetSearchInfo(params)
+        if (result.code == 200) {
+            commit("REQGETSEARCHLIST", result.data)
+        }
+    }
 }
+// 为了简化数据
 const getters = {
-
+    //当没有获取到数据的时候就让它为一个空数组
+    goodsList(state) {
+        return state.searchList.goodsList || []
+    },
+    attrsList(state) {
+        return state.searchList.attrsList || []
+    },
+    trademarkList(state) {
+        return state.searchList.trademarkList || []
+    },
 }
 export default {
     state,
